c5cf697e6cd6e4980593719440f1055c03f42e72
artificialbutter <59352535+artificialbutter@users.noreply.github.com>
2023-11-03 10:23:53 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+python backend for spongegpt so it can use more ML models
+
+i do not know python
---
+++ main.py
@@ -0,0 +1,23 @@
+# keep in mind i have no idea how python works
+import socketio
+import aiohttp
+from transformers import pipeline
+
+image_to_text = pipeline("image-to-text", model="nlpconnect/vit-gpt2-image-captioning")
+
+# create a Socket.IO server
+sio = socketio.Server()
+
+@sio.event
+def imgcaption(sid, data):
+    print('message ', data)
+    caption = image_to_text(data)
+    return caption, 200
+
+@sio.event
+def connect(sid, environ, auth):
+    print('connect ', sid)
+
+@sio.event
+def disconnect(sid):
+    print('disconnect ', sid)
