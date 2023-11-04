97af4d6b7240df5d8803acecedb0f3ef5a4d144e
artificial <59352535+artificialbutter@users.noreply.github.com>
2023-11-03 19:12:47 -0700

    i can make it slower

--- main.py
+++ main.py
@@ -3,14 +3,13 @@ import socketio
 import eventlet
 from transformers import pipeline
 
-image_to_text = pipeline("image-to-text", model="nlpconnect/vit-gpt2-image-captioning")
-
 # create a Socket.IO server
 sio = socketio.Server()
 app = socketio.WSGIApp(sio)
 
 @sio.event
 def imgcaption(sid, data):
+    image_to_text = pipeline("image-to-text", model="nlpconnect/vit-gpt2-image-captioning")
     caption = image_to_text(data)
     return caption, 200
 
