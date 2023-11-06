1a696c35a5f5a752746fe4d949fb80e706a3cad7
artificial <59352535+artificialbutter@users.noreply.github.com>
2023-11-06 14:09:31 -0800

    characterai

---
+++ llm.py
@@ -0,0 +1,24 @@
+from characterai import PyCAI
+import os
+
+client = PyCAI(os.getenv("CHARACTERAI_TOKEN"))
+
+char = 'UNIA7ldJdQ3xxNQD6hGDBmuBJcr1gCyIK-xliu5C0gM'
+
+chat = client.chat.get_chat(char)
+
+participants = chat['participants']
+
+if not participants[0]['is_human']:
+    tgt = participants[0]['user']['username']
+else:
+    tgt = participants[1]['user']['username']
+
+def send_message(message):
+    data = client.chat.send_message(
+        chat['external_id'], tgt, message
+    )
+
+    text = data['replies'][0]['text']
+
+    return text
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import socketio
 import eventlet
 
 from transformers import pipeline
+from llm import send_message
 
 # create the Socket.IO server
 sio = socketio.Server()
@@ -12,6 +13,10 @@ def imgcaption(sid, data):
     image_to_text = pipeline("image-to-text", model="nlpconnect/vit-gpt2-image-captioning")
     caption = image_to_text(data)
     return caption, 200
+
+@sio.event
+def chat(sid, data):
+    return send_message(data), 200
 
 @sio.event
 def connect(sid, environ, auth):
