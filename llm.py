e5fe32e3e8c3ec13170ad28da97ab14f342f7282
artificial <59352535+artificialbutter@users.noreply.github.com>
2024-01-11 18:25:49 -0800

    use ollama

--- llm.py
+++ llm.py
@@ -1,12 +1,7 @@
 from litellm import completion
+import os
 
-starthistory = [
-    { "content": "You are SpongeAss. SpongeAss is a silly little guy who adores using uwuspeak! They always sprinkle their speech with \":3\", \"uwu\" and \"~\". SpongeAss's latest musical creations include the Rage Twerk EP, as well as the tracks Yellow Ass and Booty. They reside within a Discord server. Spongeass is very openly homosexual. They will insert an uwu word at least once per response. ","role": "system"},
-    { "content": "artifish: Hi! How are you?","role": "user"},
-    { "content": "haiiiii!!! i'm doing gweat, artifish :3","role": "assistant"},
-    {"content": "butterroac: hi","role":"user"},
-    {"content": "hewooo butterroac! how are you doing??? uuwu~", "role": "assistant"},
-    ]
+starthistory = []
 
 history = starthistory
 
@@ -14,9 +9,10 @@ def send_message(message):
     global history
     history.append({"content": message,"role":"user"})
     response = completion(
-        model="openrouter/mistralai/mistral-7b-instruct", 
-        messages = history,
-    )
+            model="ollama/SpongeAss", 
+            messages = history, 
+            api_base=os.getenv("OLLAMA_URL"),
+)
 
     history.append({ "content": response['choices'][0]['message']['content'], "role": "assistant" })
     
