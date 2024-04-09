ac992c528b70fd90ba70829278ef2112300976dd
artifish <dev@arti.gay>
2024-04-08 23:13:20 -0700

    Revert "no longer"
    This reverts commit ddf232c3e58208783e326cb709ff409bc0f09efd.

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import socketio
 import eventlet
 
 from transformers import pipeline
+from apiCAI import send_message, new_chat
+from llm import send_message as LLMmessage, reset as LLMreset
 
 # create the Socket.IO server
 sio = socketio.Server()
@@ -12,6 +14,18 @@ def imgcaption(sid, data):
     image_to_text = pipeline("image-to-text", model="nlpconnect/vit-gpt2-image-captioning")
     caption = image_to_text(data)
     return caption, 200
+
+@sio.event
+def chat(sid, data):
+    if data['usellm'] == True:
+        return LLMmessage(data['message']), 200
+    else:     
+        return send_message(data['message']), 200
+
+@sio.event
+def newchat(sid, data):
+    LLMreset()
+    return new_chat(), 200
 
 @sio.event
 def connect(sid, environ, auth):
