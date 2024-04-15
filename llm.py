41245c62afef04bcee8ede2db7bbe1667063db57
artificialbutter <59352535+artificialbutter@users.noreply.github.com>
2024-04-15 12:05:30 -0700

    Revert "Reapply "no longer""
    This reverts commit dee57a0baf03aee7c30529364723afb62a32d3f1.

---
+++ llm.py
@@ -0,0 +1,23 @@
+from litellm import completion
+import os
+
+starthistory = []
+
+history = starthistory
+
+def send_message(message):
+    global history
+    history.append({"content": message,"role":"user"})
+    response = completion(
+            model="ollama/SpongeAss:latest", 
+            messages = history, 
+            api_base=os.getenv("OLLAMA_URL"),
+)
+
+    history.append({ "content": response['choices'][0]['message']['content'], "role": "assistant" })
+    
+    return response['choices'][0]['message']['content']
+
+def reset():
+    global history
+    history = starthistory
