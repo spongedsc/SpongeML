a911ab1cd081dafbd81fd6f3cd652705b1d059ec
artifishvr <dev@arti.gay>
2024-04-20 22:07:09 -0700

    get rid of unused litellm

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import spongelang
 
 from transformers import pipeline
 from apiCAI import send_message, new_chat
-from llm import send_message as LLMmessage, reset as LLMreset
 
 # create the Socket.IO server
 sio = socketio.Server()
@@ -14,10 +13,8 @@ app = socketio.WSGIApp(sio)
 
 @sio.event
 def imgcaption(sid, data):
-    if (
-        psutil.virtual_memory().available / 1048576 < 2000
-    ):  # check if available ram is less than 2gb (2GB != 2048MB btw 2GiB == 2048MiB GB != GiB MB != MiB you're just an idiot)
-        print("THERE'S LESS THAN 2GB RAM HELP")
+    if psutil.virtual_memory().available / 1048576 < 3000:
+        print("Image Recognition Skipped due to low memory")
         return "", 200
     image_to_text = pipeline(
         "image-to-text", model="nlpconnect/vit-gpt2-image-captioning"
@@ -28,23 +25,19 @@ def imgcaption(sid, data):
 
 @sio.event
 def chat(sid, data):
-    if data["usellm"] == True:
-        return LLMmessage(data["message"]), 200
-    else:
-        msg = send_message(send_message(data["message"]))
-        code = spongelang.extract_spongelang(msg)
-        if code:
-            try:
-                out = spongelang.process_function_calls(code, spongelang.functions)
-            except Exception as e:
-                out = f"ERROR! {type(e).__name__}: {e}"
-            return msg + f"\n\nSpongeLang output:\n\n```\n{out}\n```", 200
-        return msg, 200
+    msg = send_message(send_message(data["message"]))
+    code = spongelang.extract_spongelang(msg)
+    if code:
+        try:
+            out = spongelang.process_function_calls(code, spongelang.functions)
+        except Exception as e:
+            out = f"ERROR! {type(e).__name__}: {e}"
+        return msg + f"\n\nSpongeLang output:\n\n```\n{out}\n```", 200
+    return msg, 200
 
 
 @sio.event
 def newchat(sid, data):
-    LLMreset()
     return new_chat(), 200
 
 
